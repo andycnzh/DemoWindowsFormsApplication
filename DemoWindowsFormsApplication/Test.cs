59346e59cfd29afd8980ebac8eb779242f47c6b9
andycnzh <andycnzh@outlook.com>
2014-08-18 20:36:46 +0800

    initial

---
+++ DemoWindowsFormsApplication/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoWindowsFormsApplication
+{
+    class Test
+    {
+        public delegate void UpdateMessageEventHandler(string message);
+        public event UpdateMessageEventHandler UpdateMessageEvent;
+
+        public void A()
+        {
+            UpdateMessageEvent("This is from A");
+
+            B();
+        }
+
+        public void B()
+        {
+            UpdateMessageEvent("This is from B");
+        }
+    }
+}
---
+++ DemoWindowsFormsApplication/fmMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DemoWindowsFormsApplication
+{
+    public partial class fmMain : Form
+    {
+        Test t = new Test();
+
+        public fmMain()
+        {
+            InitializeComponent();
+
+            t.UpdateMessageEvent += UpdateMessage;
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            UpdateMessage("Click button");
+
+            t.A();
+        }
+
+        private void UpdateMessage(string message)
+        {
+            txtMessage.Text += Environment.NewLine + message;
+        }
+    }
+}
